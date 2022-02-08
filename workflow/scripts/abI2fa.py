04436cf2304bddb7975ee63c9ad28660dd67b6a1
Your Name <etholleman@ucdavis.edu>
2022-02-07 17:13:16 -0800

    format with black

--- workflow/scripts/abI2fa.py
+++ workflow/scripts/abI2fa.py
@@ -1,12 +1,14 @@
 from Bio import SeqIO
 
+
 def main():
-    
+
     abi = str(snakemake.input)
     fasta = str(snakemake.output)
-    
-    record = SeqIO.read(abi, 'abi')
-    SeqIO.write(record, fasta, 'fasta')
 
-if __name__ == '__main__':
+    record = SeqIO.read(abi, "abi")
+    SeqIO.write(record, fasta, "fasta")
+
+
+if __name__ == "__main__":
     main()
--- workflow/scripts/gb2fa.py
+++ workflow/scripts/gb2fa.py
@@ -1,12 +1,14 @@
 from Bio import SeqIO
 
-def main():
-    
-    gb = str(snakemake.params['gb_path'])
-    fa = str(snakemake.output)
-    
-    record = SeqIO.read(gb, 'gb')
-    SeqIO.write(record, fa, 'fasta')
 
-if __name__ == '__main__':
+def main():
+
+    gb = str(snakemake.params["gb_path"])
+    fa = str(snakemake.output)
+
+    record = SeqIO.read(gb, "gb")
+    SeqIO.write(record, fa, "fasta")
+
+
+if __name__ == "__main__":
     main()
